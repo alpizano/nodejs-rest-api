987285e063f5ac35716f8e1ffaed4fbc20d514da
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-11-09 21:40:46 -0600

    added api and routes subdirectories

---
+++ node-rest-shop/api/routes/products.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+// subpackage w/ capibilities to handle different routes w/ diff endpoints
+const router = express.Router();
+
+// Handles GET requests
+router.get('/')
+
--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.status(200).json({
         message: 'It works!'
     });
-}); // incoming request has to go through use
+}); 
+*/
+
+// incoming request has to go through use
+
+app.use('/products', productRoutes);
 
 module.exports = app;
