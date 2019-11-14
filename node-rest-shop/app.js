de3d63b514a2ba59d78ed5f662da90e26855830c
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-11-13 23:57:16 -0600

    added CORS error handling

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -11,6 +11,16 @@ app.use((req, res, next) => {
 }); 
 */
 
+// Set headers to handle CORS
+app.use((req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET');
+        return res.status(200).json({});
+    }
+});
+
 // Routes that handle request
 const productRoutes = require('./api/routes/products.js');
 const orderRoutes = require('./api/routes/orders.js');
