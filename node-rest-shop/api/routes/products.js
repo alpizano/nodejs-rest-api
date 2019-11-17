180ddbc5b2649605df62bd8984caba8f224e0ae7
ennth <39110859+aemaeth0@users.noreply.github.com>
2019-11-17 01:02:02 -0600

    fixed schema instantiation in models//products.js and created post method for products to save in MongoDB

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 // subpackage w/ capibilities to handle different routes w/ diff endpoints
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/products.js');
 
 // Handles GET requests
 router.get('/', (req,res,next) => {
@@ -11,10 +14,19 @@ router.get('/', (req,res,next) => {
 });
 
 router.post('/', (req,res,next) => {
-    const product = {
+  // Constructor 
+    const product = Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    });
+
+    // Stores into database
+    product.save().then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
+
     res.status(201).json({
         message: 'Handling POST requests to /products',
         createdProduct: product
